941a65f84735f1319bc78ccbd2978c07a02a913a
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-28 22:51:41 +0530

    Post API call using axios completed

--- DemoProject/App.js
+++ DemoProject/App.js
@@ -8,15 +8,45 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+  Pressable,
   View,
+  StyleSheet
 } from 'react-native';
+import axios from 'axios';
+
 function App() {
-  return <Text>Demo App</Text>
+  const [advice, setAdvice] = React.useState('No Advice');
+
+  const getRandomId = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return (Math.floor(Math.random() * (max - min + 1)) + min).toString();
+  };
+
+  const getAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice/' + getRandomId(1, 200))
+      .then(response => {
+        setAdvice(response.data.slip.advice);
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  };
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>{advice}</Text>
+      <View style={{padding: 20, backgroundColor: 'red', borderRadius:10, marginTop: 50}}>
+      <Pressable onPress={() => getAdvice()}>
+        <Text>
+          Get Advice
+        </Text>
+      </Pressable>
+      </View>
+    </View>
+  );
 }
 export default App;
+
+
