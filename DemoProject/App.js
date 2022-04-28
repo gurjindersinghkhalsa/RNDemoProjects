a38343bae2afcd51ceb90dac82787c07b78055b7
gurjindersingh <gurjindersingh663@gmail.com>
2022-04-28 22:32:27 +0530

    initial commit - project created

---
+++ DemoProject/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+function App() {
+  return <Text>Demo App</Text>
+}
+export default App;
