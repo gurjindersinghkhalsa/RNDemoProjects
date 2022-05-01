d3555e5fdc2f8ba894cd02b0534c1ab2dfeb4f1a
gurjindersingh <gurjindersingh663@gmail.com>
2022-05-01 23:33:48 +0530

    api call added

--- DemoProject/App.js
+++ DemoProject/App.js
@@ -6,17 +6,48 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Pressable,
 } from 'react-native';
 function App() {
-  return <Text>Demo App</Text>
+  const [data, setData] = useState([]);
+
+  async function getData() {
+    try {
+    const response = await fetch('https://reactnative.dev/movies.json');
+    const data = await response.json();
+    setData(data.movies);
+    } catch(error){
+      alert(error)
+    }
+  }
+  function resetData(){
+    setData([])
+  }
+  return (
+    <View style={style.root}>
+      {data && data.map(model => <Text key={model.title}>{model.title}</Text>)}
+      <Pressable onPress={getData}>
+        <Text>Get Movies Name</Text>
+      </Pressable>
+      <Pressable onPress={resetData}>
+        <Text>Reset</Text>
+      </Pressable>
+    </View>
+  );
 }
 export default App;
+const style = StyleSheet.create({
+  opacity: {
+    opacity: 0.75,
+  },
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
