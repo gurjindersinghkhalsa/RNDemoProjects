0597b64ec5d969087472d0f0dd6c85958e64db1b
gurjindersingh <gurjindersingh663@gmail.com>
2022-06-06 22:05:50 +0530

    state updated

--- DemoProject/App.js
+++ DemoProject/App.js
@@ -13,10 +13,36 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  Pressable,
   View,
 } from 'react-native';
-function App() {
-  return <Text>Demo App</Text>
+// function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+    };
+  }
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Pressable
+          onPress={() => {
+            this.setState({value: this.state.value + 1}, () => {
+              console.log('Increment', this.state.value);
+            });
+          }}>
+          <Text
+            style={{backgroundColor: 'pink', padding: 15, fontWeight: '600'}}>
+            Increment
+          </Text>
+        </Pressable>
+        <Text
+        style={{padding: 15, fontWeight: '600'}}>
+          Vale is {this.state.value}</Text>
+      </View>
+    );
+  }
 }
 export default App;
