aa4503ffe17b77da9337457d1828e067be666367
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-28 23:59:06 +0530

    work completed

--- DemoProject/App.js
+++ DemoProject/App.js
@@ -6,52 +6,33 @@
  * @flow strict-local
  */
 
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
-import Student from './Student';
-class App extends Component {
-  constructor() {
-    super(); // calling parent calls constructor
-    this.state = { // state obj
-      name: 'Gurjinder Singh', // this is state
-    };
-    // creates function once per component instance prefer this one
-    // this.changeName = this.changeName.bind(this) // for update state on button click
-  }
-  updateName(val) {
-    this.setState({name: val})
-  }
-  changeName() {
-    this.setState({name: 'on click update'})
-  }
-  render() {
-    return (
-      <View style={{backgroundColor: 'white', flex: 1, marginTop: 120}}>
-        <Text>Hi Friends {this.state.name}</Text>
-        <TextInput
-          placeholder="Enter you name"
-          onChangeText={val => this.updateName(val)}
-        />
+import React, {useEffect, useState} from 'react';
+import {Text, View, Button} from 'react-native';
+const App = () => {
 
-        <Button
-          title="Press me"
-          style={{backgroundColor: 'red'}}
-          /* creates new function on every render call so avoid*/
-          onPress={()=>this.changeName()}
-            //OR
-          // onPress={this.changeName.bind(this)}
-        />
-        <Student name={this.state.name}/>
-      </View>
-    );
-  }
+  const [show, setshow] = useState(false)
+  return (
+    <View style={{backgroundColor: 'white', flex: 1, marginTop: 100}}>
+      <Button title="Toggle Component" onPress={() => setshow(!show)} />
+      { show ? <StudentP name='Gurjinder Singh'/> : null }
+    </View>
+  );
+};
+
+const StudentP=(props)=>{
+  let timer = setInterval(() => {
+    console.warn('Timere');
+  }, 2000); // This will call every 2 sec
+
+  useEffect(()=>{
+    return ()=> {
+      clearInterval(timer);
+    }
+  })
+  return(
+    <View>
+      <Text style={{backgroundColor:'red'}}>{props.name}</Text>
+    </View>
+  )
 }
-
-const style = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    marginTop: 70,
-    backgroundColor: 'white',
-  },
-});
 export default App;
